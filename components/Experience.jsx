51d3b0ec62d147ea4112fd25c002ec8cdab7e308
PickleBoxer <matic.vertacnik@icloud.com>
2023-02-24 18:35:52 +0100

    Add model to Experience

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -2,6 +2,7 @@
   /* add OrbitControls from React Three Drei */
 }
 import { OrbitControls } from "@react-three/drei";
+import { Office } from "/components/WawaOffice";
 
 export const Experience = () => {
   return (
@@ -9,7 +10,11 @@ export const Experience = () => {
       <OrbitControls />
       <mesh>
         {/* BoxGeometry is a geometry class for a rectangular cuboid */}
-        <boxGeometry />
+        {/*<boxGeometry /> */}
+        {/* Office Component 3D Model */}
+        <Office />
+        {/* ambientLight to avoid seeing the model in black */}
+        <ambientLight intensity={.5}/>
         {/* A material that maps the normal vectors to RGB colors */}
         <meshNormalMaterial />
       </mesh>
