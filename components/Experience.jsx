5005b7bb0ead8c73c46282091c3872d40f88789b
PickleBoxer <matic.vertacnik@icloud.com>
2023-02-24 19:06:20 +0100

    Add ScrollControls, disable OrbitControls Zoom

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,18 +1,23 @@
 {
   /* add OrbitControls from React Three Drei */
 }
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, ScrollControls } from "@react-three/drei";
 import { Office } from "/components/WawaOffice";
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
+      {/* OrbitControls are catching the scroll event, disable by enableZoom={false} */}
+      <OrbitControls enableZoom={false} />
       <mesh>
         {/* BoxGeometry is a geometry class for a rectangular cuboid */}
         {/*<boxGeometry /> */}
+        {/* Scroll controls create a HTML scroll container in front of the canvas */}
+        <ScrollControls pages={3} damping={0.25}>
+        {/* Canvas contents in here will *not* scroll, but receive useScroll! */}
         {/* Office Component 3D Model */}
         <Office />
+        </ScrollControls>
         {/* ambientLight to avoid seeing the model in black */}
         <ambientLight intensity={.5}/>
         {/* A material that maps the normal vectors to RGB colors */}
